98bf099996577f965a19593649548c92074c43f6
Cianan <B00164511@mytudublin.ie>
2025-12-01 14:33:45 +0000

    PrayerList

---
+++ home.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Assignment Tracker</title>
+</head>
+<body>
+
+<h1>Home</h1>
+
+<p>Add and view Assignments</p>
+
+<p><a href="addAssignment.php">Add a New Assignment</a></p>
+<p><a href="viewAssignment.php">View All Assignments</a></p>
+
+</body>
+</html>
